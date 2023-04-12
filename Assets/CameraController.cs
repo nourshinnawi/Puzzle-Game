c5c14d174e1ee8e5b315ec0431f1a57887dca561
Nour <50388570+nourshinnawi@users.noreply.github.com>
2023-04-12 06:41:21 -0700

    Add files via upload

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject cameraDock = null;
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraDock.transform.position, Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Feeler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feeler : MonoBehaviour
+{
+    public bool isValid = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "GridSpace")
+        {
+            isValid = true;
+            gameObject.GetComponent<Renderer>().enabled = true;
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (other.gameObject.tag == "GridSpace")
+        {
+            isValid = false;
+            gameObject.GetComponent<Renderer>().enabled = false;
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
